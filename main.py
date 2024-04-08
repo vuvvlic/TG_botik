088d92a2cd582a4c94bc16d9beb183165fa10558
vuvvlic <123775560+vuvvlic@users.noreply.github.com>
2024-04-08 18:09:48 +0300

    andrei

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import sqlite3
 
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler
@@ -15,13 +16,25 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 
 async def start(update, context):
+    user_id = update.message.from_user.id
+    user_name = update.message.from_user.first_name
     await update.message.reply_text(
-        "Привет",
+        f"Привет {user_name}",
         reply_markup=markup
     )
+    con = sqlite3.connect('baza_tg_bot')
+    if__ = f"""INSERT INTO users VALUES({user_id}, "{user_name}")"""
+    cur = con.cursor()
+    cur.execute(if__)
+    con.commit()
+    con.close()
 
 
 def main():
-    application = Application.builder().token('BOT_TOKEN').build()
+    application = Application.builder().token('6634204145:AAHZhQ_XCdTct4Gir-BqP0P1XSaEyw8Ytgs').build()
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
