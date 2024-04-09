3dcac4f540e77618705c8e55539f715e6adce5ab
vuvvlic <123775560+vuvvlic@users.noreply.github.com>
2024-04-09 23:05:59 +0300

    andrei

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import sqlite3
 
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -13,6 +13,11 @@ logger = logging.getLogger(__name__)
 reply_keyboard = [['/interpreter', '/collection_of_formulas'],
                   ['/tests', '/task_list']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+reply_keyboard1 = [['/phis', '/math'],
+                   ['/stop_formuls']]
+markup_formul = ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=False)
+
+school_object = False
 
 
 async def start(update, context):
@@ -30,9 +35,45 @@ async def start(update, context):
     con.close()
 
 
+async def collection_of_formulas(update, context):
+    global markup_formul
+    await update.message.reply_text('Выбери предмет', reply_markup=markup_formul)
+
+
+async def phis(update, context):
+    global school_object
+    school_object = True
+
+
+async def math__(update, context):
+    global school_object
+    school_object = True
+
+
+async def stop_formuls(update, context):
+    global markup
+    global school_object
+    school_object = False
+    await update.message.reply_text('OK',
+                                    reply_markup=markup
+                                    )
+
+
+async def echo_formul(update, context):
+    global school_object
+    if school_object:
+        await update.message.reply_text(f"формула")
+
+
 def main():
     application = Application.builder().token('6634204145:AAHZhQ_XCdTct4Gir-BqP0P1XSaEyw8Ytgs').build()
+    text_formul = MessageHandler(filters.TEXT, echo_formul)
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("phis", phis))
+    application.add_handler(CommandHandler("math", math__))
+    application.add_handler(CommandHandler("stop_formuls", stop_formuls))
+    application.add_handler(CommandHandler("collection_of_formulas", collection_of_formulas))
+    application.add_handler(text_formul)
     application.run_polling()
 
 
